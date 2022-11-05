574d521bc255065d22b5b01c306923a30d7d2b7e
HSN-Studio <hassanrafique198@gmail.com>
2022-11-05 14:59:35 +0500

    Started the project, +Sass installed!

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logoshogo from "../images/logo.svg";
+function Header() {
+  return (
+    <header className="header">
+      <img src={logoshogo} alt="logo" />
+    </header>
+  );
+}
+
+export default Header;
